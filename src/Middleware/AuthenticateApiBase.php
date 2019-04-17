3f842b15142b7c740f815dea3cbd3c3892b5a096
fmelchor <fmelchor@intecfra.com>
2019-04-17 10:43:28 -0500

    agregando middleware

---
+++ src/Middleware/AuthenticateApiBase.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrador
+ * Date: 17/04/19
+ * Time: 10:13 AM
+ */
+namespace ApiBase\Middleware;
+use ApiBase\utils\Token;
+use Closure;
+use Carbon\Carbon;
+use ApiBase\Traits\ReplyTrait;
+use ApiBase\Responses\Responses;
+class AuthenticateApiBase
+{
+    use ReplyTrait;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $oResponse = new Responses();
+        $tokenUser = $request->header('Authorization');
+        if ($tokenUser == null) {
+            $oResponse->status = 0;
+            $oResponse->message = "El token es requerido";
+            return $this->Unauthorized($oResponse);
+        }
+        $user = User::where('Token', '=', $tokenUser)->get()->first();
+        if ($user == null) {
+            $oResponse->status = 0;
+            $oResponse->message = "No tienes permiso para este recurso";
+            return $this->Unauthorized($oResponse);
+        }
+        //para checar la cadusidad del token
+       $tokenEncode = Token::tokenDecode($tokenUser);
+        $dt = new \DateTime($tokenEncode->fecha);
+        $carbon = Carbon::instance($dt);
+        if ($carbon->diffInHours(Carbon::now()) >= 1) {
+            $user->token = "";
+            $user->save();
+            $oResponse->status = 0;
+            $oResponse->message =  "el token a caducado";
+            return $this->Unauthorized($oResponse);
+        }
+        return $next($request);
+    }
+}
---
+++ src/utils/Token.php
@@ -0,0 +1,27 @@
+<?php
+namespace ApiBase\utils;
+
+use Carbon\Carbon;
+use Firebase\JWT\JWT;
+use App\User;
+use Illuminate\Support\Facades\Config;
+class Token
+{
+    public static function tonkenEncode(User $usuario){
+        $key = Config::get('app.jwtKey');
+        $token = array(
+            "nombre" => $usuario->nombre,
+            "email" => $usuario->email,
+            "fecha" => Carbon::now()->toDateTimeString()
+        );
+
+        $jwt = JWT::encode($token,$key);
+        return $jwt;
+
+    }
+    public static function tokenDecode($jwt){
+        $key = Config::get('app.jwtKey');
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+        return $decoded;
+    }
+}
